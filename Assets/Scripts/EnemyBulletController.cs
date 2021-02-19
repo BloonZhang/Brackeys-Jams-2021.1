54cc05681b9746b017b1b5acccff7e48e780a779
Bloon <BloonZhang@gmail.com>
2021-02-18 22:58:08 -0600

    Player damage

--- Assets/Scripts/EnemyBulletController.cs
+++ Assets/Scripts/EnemyBulletController.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class EnemyBulletController : MonoBehaviour
 {
+    // abbreviation variables
+    private int playerHitboxLayer = 13;
 
     // definition variables (default values)
     private float timeOnScreen = 5f;
+    private int damage = 1;
 
     // helper variables
     private float timer = 0f;
@@ -22,12 +25,25 @@ public class EnemyBulletController : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
-        if (timer > timeOnScreen) { Destroy(this.gameObject); }
+        if (timer > timeOnScreen) { RemoveBullet(); }
     }
 
     // "constructors"
     public void DefineBulletLifetime(float time)
     {
         timeOnScreen = time;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        // if it hits the player hitbox
+        if(col.gameObject.layer == playerHitboxLayer) { PlayerController.Instance.TakeDamage(damage); }
+        RemoveBullet();
+    }
+
+    // helper methods
+    private void RemoveBullet()
+    {
+        Destroy(this.gameObject);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,11 +10,22 @@ public class PlayerController : MonoBehaviour
     public static PlayerController Instance { get {return _instance;} }
     //////// Singleton shenanigans continue in Awake() ////
 
+    // GameObjects
+    [SerializeField] private TextMeshProUGUI hpText;
+
+    // game properties
+    [SerializeField] private int maxHealth;
+    private int currentHealth;
+    public int CurrentHealth {get { return currentHealth; } }
+
     void Awake()
     {
         // Singleton shenanigans
         if (_instance != null && _instance != this) {Destroy(this.gameObject);} // no duplicates
         else {_instance = this;}
+
+        // Set up variables
+        currentHealth = maxHealth;
     }
 
 
@@ -26,12 +38,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        hpText.text = "HP: " + currentHealth + "/" + maxHealth;
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
         //Debug.Log(col.gameObject.name);
+    }
+
+    // public methods
+    public void TakeDamage(int damage)
+    {
+        Debug.Log("ow");
+        currentHealth -= damage;
     }
 
 }
